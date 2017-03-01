e8e6e342f294a1287780909bf131879df02e63b8
lalala <1395376546@qq.com>
2017-03-01 14:56:29 +0800

    wrote a mustknow_git file

---
+++ mustknow_git.js
@@ -0,0 +1,4 @@
+1.教程 http://www.liaoxuefeng.com/wiki/0013739516305929606dd18361248578c67b8067c8c017b000/0013743256916071d599b3aed534aaab22a0db6c4e07fd0000
+2.》创建库‘
+    进入目录(最好不要用中文路径)
+    git init  
